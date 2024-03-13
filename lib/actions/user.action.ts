8df72654c3ca5fc354ef36ba3b9ff8054d483bec
Vidit Shah <vidit0210@protonmail.com>
2024-03-13 16:12:34 +0530

    DynamoDBActions

---
+++ lib/actions/user.action.ts
@@ -0,0 +1,46 @@
+"use server";
+import {
+  DynamoDBClient,
+  PutItemCommand,
+  UpdateItemCommand,
+  DeleteItemCommand,
+} from "@aws-sdk/client-dynamodb";
+import { marshall } from "@aws-sdk/util-dynamodb";
+
+export const createUser = async ({
+  clerkId,
+  name,
+  username,
+  email,
+  picture,
+}: {
+  clerkId: string;
+  name: string;
+  username: string;
+  email: string;
+  picture: string;
+}) => {
+  const ddbClient = new DynamoDBClient({
+    region: process.env.AWS_REGION as string,
+    credentials: {
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID as string,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY as string,
+    },
+  });
+  const params = {
+    TableName: "Users",
+    Item: marshall({
+      ClerkID: clerkId,
+      Name: name,
+      Username: username,
+      Email: email,
+      Picture: picture,
+    }),
+  };
+
+  try {
+    await ddbClient.send(new PutItemCommand(params));
+  } catch (err) {
+    throw err;
+  }
+};
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    mdxRs: true,
+    serverComponentsExternalPackages: [
+      "@aws-sdk/client-dynamodb",
+      "@aws-sdk/util-dynamodb",
+    ],
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*",
+      },
+      {
+        protocol: "http",
+        hostname: "*",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
