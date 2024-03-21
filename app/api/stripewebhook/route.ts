72c5f7cf2777c0b7e006b8d057a2e5285c929832
Vidit Shah <vidit0210@protonmail.com>
2024-03-21 10:00:33 +0530

    Added Stripe Webhook

---
+++ app/api/stripewebhook/route.ts
@@ -0,0 +1,38 @@
+import Stripe from "stripe";
+import { headers } from "next/headers";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+
+export async function POST(req: Request) {
+  const body = await req.text();
+  const sig = headers().get("Stripe-Signature") as string;
+  const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET!;
+  let event: Stripe.Event;
+
+  try {
+    if (!sig || !webhookSecret) return;
+    event = stripe.webhooks.constructEvent(body, sig, webhookSecret);
+  } catch (err: any) {
+    console.log(`❌ Error message: ${err.message}`);
+    return new Response(`Webhook Error: ${err.message}`, { status: 400 });
+  }
+
+  try {
+    switch (event.type) {
+      case "payment_intent.succeeded":
+        // Add Credits to The  User
+        break;
+      case "product.updated":
+        break;
+      default:
+        throw new Error("Unhandled relevant event!");
+    }
+  } catch (error) {
+    console.log(error);
+    return new Response("Webhook handler failed. View logs.", {
+      status: 400,
+    });
+  }
+
+  return new Response(JSON.stringify({ received: true }));
+}
