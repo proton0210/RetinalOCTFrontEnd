f8fd5d65bdd4bb73765a733312425cecc2ef12c6
Vidit Shah <vidit0210@protonmail.com>
2024-03-21 18:25:00 +0530

    Passing the Clerk ID

--- app/api/payment/route.ts
+++ app/api/payment/route.ts
@@ -1,5 +1,31 @@
+// import Stripe from "stripe";
+// import { NextResponse, NextRequest } from "next/server";
+
+// export async function POST(request: Request) {
+//   const STRIPE_SECRET_KEY = process.env.STRIPE_SECRET_KEY;
+//   if (!STRIPE_SECRET_KEY) {
+//     throw new Error("Please add STRIPE_SECRET_KEY to .env");
+//   }
+//   const stripe = new Stripe(STRIPE_SECRET_KEY);
+//   let data = await request.json();
+//   let priceId = data.priceId;
+//   const session = await stripe.checkout.sessions.create({
+//     line_items: [
+//       {
+//         price: priceId,
+//         quantity: 1,
+//       },
+//     ],
+//     mode: "payment",
+//     success_url: "http://localhost:3000",
+//     cancel_url: "http://localhost:3000",
+//   });
+
+//   return NextResponse.json(session.url);
+// }
+
 import Stripe from "stripe";
-import { NextResponse, NextRequest } from "next/server";
+import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
   const STRIPE_SECRET_KEY = process.env.STRIPE_SECRET_KEY;
@@ -7,8 +33,13 @@ export async function POST(request: Request) {
     throw new Error("Please add STRIPE_SECRET_KEY to .env");
   }
   const stripe = new Stripe(STRIPE_SECRET_KEY);
-  let data = await request.json();
-  let priceId = data.priceId;
+
+  // Get the request body
+  const data = await request.json();
+
+  // Extract the priceId and clerkId from the request body
+  const { priceId, clerkId } = data;
+
   const session = await stripe.checkout.sessions.create({
     line_items: [
       {
@@ -19,6 +50,9 @@ export async function POST(request: Request) {
     mode: "payment",
     success_url: "http://localhost:3000",
     cancel_url: "http://localhost:3000",
+    metadata: {
+      userId: clerkId, // Pass the Clerk ID as part of the session metadata
+    },
   });
 
   return NextResponse.json(session.url);
